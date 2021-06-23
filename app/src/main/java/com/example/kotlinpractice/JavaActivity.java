f49c2187b0eced84885565f277109207414c5895
wsb7788 <wsb0514@naver.com>
2021-06-24 02:18:59 +0900

    up

---
+++ app/src/main/java/com/example/kotlinpractice/JavaActivity.java
@@ -0,0 +1,13 @@
+package com.example.kotlinpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JavaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_java);
+    }
+}
--- app/src/main/java/com/example/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpractice/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.kotlinpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {        //물음표 - nullable
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+/*        sum(1,2f)
+
+        println(describe(1))
+
+        val fruits = listOf("banana", "avocado", "apple", "kiwifruit")
+        fruits
+            .filter{it.startsWith("a")}
+            .sortedBy { it }
+            .map{it.toUpperCase()}
+            .forEach{println(it)}*/
+
+        btn.setOnClickListener{
+            Toast.makeText(this,"asdf",Toast.LENGTH_SHORT)
+        }
     }
+    fun sum(a: Int, b: Float) {
+        println("$a 와 $b 의 합은 ${a+b}")
+    }
+
+    fun parseInt(str: String):Int?{
+        var maNum: Int? = null
+
+        try{maNum = str.toInt()}
+        catch(nfe : NumberFormatException){return null}
+        return maNum
+    }
+    fun describe(obj: Any): String =
+        when(obj){              //해당되는게 있으면 실행 후 나감
+            1 -> "One"
+            "Hello" -> "Greeting"
+            is Long -> "Long"
+            !is String -> "Not a String"
+            else -> "Unknown"
+        }
+
+
 }
