b3deddced02c49940a90199c33f607f82d3ed87c
wsb7788 <wsb0514@naver.com>
2021-06-25 03:05:59 +0900

    Login kotlin code

--- app/src/main/java/com/example/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpractice/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.kotlinpractice
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethod
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.NumberFormatException
@@ -22,11 +26,33 @@ class MainActivity : AppCompatActivity() {
             .sortedBy { it }
             .map{it.toUpperCase()}
             .forEach{println(it)}*/
-
+/*
         btn.setOnClickListener{
             Toast.makeText(this,"asdf",Toast.LENGTH_SHORT).show()
+        }*/
+        et_email.imeOptions = EditorInfo.IME_ACTION_DONE
+
+        // 마지막 에딧텍스트 완료버튼은 Login 호출
+        et_age.setOnEditorActionListener() { v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_DONE){
+                Login(v)
+                true
+            } else false
         }
     }
+
+    fun Login(v: View){
+        // 키보드 숨기기
+        var imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(v.windowToken, 0)
+
+        // 입력정보 비교
+        if(et_email.text.toString() == "wsb0514@naver.com" && et_password.text.toString() =="1234" && et_name.text.toString() == "가나다" && et_age.text.toString() == "24")
+            Toast.makeText(this, "로그인 성공!!", Toast.LENGTH_SHORT).show()
+        else Toast.makeText(this, "로그인 실패 ㅠㅠ", Toast.LENGTH_SHORT).show()
+    }
+
+
     fun sum(a: Int, b: Float) {
         println("$a 와 $b 의 합은 ${a+b}")
     }
