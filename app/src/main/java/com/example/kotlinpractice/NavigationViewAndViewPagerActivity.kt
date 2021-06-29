04b8b68c739264a074bd81e2fd9ccf5f448d42b2
wsb7788 <wsb0514@naver.com>
2021-06-30 00:25:15 +0900

    뷰페이저 java코드(오류) 가 아니라 구글에서 뷰페이저로 네비게이션 뷰 메뉴 전환 하지말라고 명시함.

--- app/src/main/java/com/example/kotlinpractice/NavigationViewAndViewPagerActivity.kt
+++ app/src/main/java/com/example/kotlinpractice/NavigationViewAndViewPagerActivity.kt
@@ -1,10 +1,15 @@
 package com.example.kotlinpractice
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import kotlinx.android.synthetic.main.activity_navigationviewandviewpager.*
 
 
@@ -15,15 +20,22 @@ class NavigationViewAndViewPagerActivity:AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigationviewandviewpager)
 
+
+
         viewList.add(layoutInflater.inflate(R.layout.fragment_home,null))
         viewList.add(layoutInflater.inflate(R.layout.fragment_favorite,null))
         viewList.add(layoutInflater.inflate(R.layout.fragment_profile,null))
 
-        viewPager.adapter = pagerAdapter()
+        viewPager.adapter = ViewPagerAdapter(getViewList())
+
+        fun getViewList(): ArrayList<Int> = arrayListOf<Int>(R.layout.fragment_home,R.layout.fragment_favorite,R.layout.fragment_profile)
+
+
+
 
     }
 
-    inner class pagerAdapter: PagerAdapter()                   //클래스 안에 만들기 때문에 inner키워드 사용. 밖에 빼도 되지만 viewList를 바로 사용하기 위해
+/*    inner class pagerAdapter: PagerAdapter()                   //클래스 안에 만들기 때문에 inner키워드 사용. 밖에 빼도 되지만 viewList를 바로 사용하기 위해
     {
         override fun getCount() = viewList.size
 
@@ -36,6 +48,41 @@ class NavigationViewAndViewPagerActivity:AppCompatActivity() {
 
         override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
             viewPager.removeView(`object` as View)
+        }
+    }*/
+
+    inner class ViewPageraAdapter(idolList: ArrayList<Int>) : RecyclerView.Adapter<ViewPagerAdapter.PagerViewHolder>() {
+        var item = idolList
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder((parent))
+
+        override fun getItemCount(): Int = item.size
+
+        override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+            holder.idol.setImageResource(item[position])
+        }
+
+        inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder
+            (LayoutInflater.from(parent.context).inflate(R.layout.idol_list_item, parent, false)){
+
+            val idol = itemView.imageView_idol!!
+        }
+    }
+
+    inner class ViewPagerAdapter(viewList: ArrayList<Int>): RecyclerView.Adapter<ViewPagerAdapter.PagerViewHolder>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): ViewPagerAdapter.PagerViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: ViewPagerAdapter.PagerViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
         }
     }
 }
