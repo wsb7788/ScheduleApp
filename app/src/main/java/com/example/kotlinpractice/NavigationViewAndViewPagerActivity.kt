f32565b4c7c23c171c36e0a7fec1d4f32ed274d4
wsb7788 <wsb0514@naver.com>
2021-06-29 23:30:42 +0900

    뷰페이저 java코드(오류)

--- app/src/main/java/com/example/kotlinpractice/NavigationViewAndViewPagerActivity.kt
+++ app/src/main/java/com/example/kotlinpractice/NavigationViewAndViewPagerActivity.kt
@@ -1,13 +1,41 @@
 package com.example.kotlinpractice
 
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.PagerAdapter
+import kotlinx.android.synthetic.main.activity_navigationviewandviewpager.*
+
 
 class NavigationViewAndViewPagerActivity:AppCompatActivity() {
+
+    var viewList = ArrayList<View>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigationviewandviewpager)
 
+        viewList.add(layoutInflater.inflate(R.layout.fragment_home,null))
+        viewList.add(layoutInflater.inflate(R.layout.fragment_favorite,null))
+        viewList.add(layoutInflater.inflate(R.layout.fragment_profile,null))
 
+        viewPager.adapter = pagerAdapter()
+
+    }
+
+    inner class pagerAdapter: PagerAdapter()                   //클래스 안에 만들기 때문에 inner키워드 사용. 밖에 빼도 되지만 viewList를 바로 사용하기 위해
+    {
+        override fun getCount() = viewList.size
+
+        override fun isViewFromObject(view: View, `object`: Any) = view == `object`
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {            //페이지를 넘길 때마다 생성
+            var curView = viewList[position]
+            viewPager.addView(curView)
+            return curView
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+            viewPager.removeView(`object` as View)
+        }
     }
 }
