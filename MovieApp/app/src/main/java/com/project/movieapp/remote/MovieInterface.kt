90ce91848070df869acf33ff1400a729457a381f
wsb7788 <wsb0514@naver.com>
2022-07-10 00:22:58 +0900

    retrofit2 적용

---
+++ MovieApp/app/src/main/java/com/project/movieapp/remote/MovieInterface.kt
@@ -0,0 +1,17 @@
+package com.project.movieapp.remote
+
+import com.project.movieapp.data.MovieResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieInterface {
+
+
+    @GET
+    suspend fun getAllMovies(
+        @Query("s")s:String,
+        @Query("page")page:Int,
+        @Query("apiKey")apiKey:String
+    ):Response<MovieResponse>
+}
---
+++ MovieApp/app/src/main/java/com/project/movieapp/utils/Constans.kt
@@ -0,0 +1,6 @@
+package com.project.movieapp.utils
+
+object Constans {
+
+    const val BASE_URL = "https://omdbapi.com/"
+}
