b13a0c62a51092b06c50f1043229873705ab6a85
wsb7788 <wsb0514@naver.com>
2022-07-10 23:03:46 +0900

    movieViewModel create

---
+++ MovieApp/app/src/main/java/com/project/movieapp/MovieViewModel.kt
@@ -0,0 +1,30 @@
+package com.project.movieapp
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import androidx.paging.liveData
+import com.project.movieapp.remote.MovieInterface
+import com.project.movieapp.ui.movie.MoviePaging
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieViewModel @Inject constructor(private val movieInterface: MovieInterface): ViewModel(){
+
+    private val query = MutableLiveData<String>("")
+
+    val list = query.switchMap { query ->
+        Pager(PagingConfig(10)){
+            MoviePaging(query,movieInterface)
+        }.liveData.cachedIn(viewModelScope)
+    }
+
+    fun setQuery(s: String){
+        query.postValue(s)
+    }
+}
--- MovieApp/app/src/main/java/com/project/movieapp/ui/movie/MovieFragment.kt
+++ MovieApp/app/src/main/java/com/project/movieapp/ui/movie/MovieFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.GridLayoutManager
+import com.project.movieapp.MovieViewModel
 import com.project.movieapp.R
 import com.project.movieapp.databinding.FragmentMovieBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,6 +19,12 @@ class MovieFragment : Fragment() {
 
 
     lateinit var binding : FragmentMovieBinding
+
+    val viewModel : MovieViewModel by viewModels()
+
+    val movieAdapter = MoviePagingAdapter()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -30,7 +41,33 @@ class MovieFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+
+        setRecyclerView()
+
+        binding.movieSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let{
+                    viewModel.setQuery(query)
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        viewModel.list.observe(viewLifecycleOwner){
+            movieAdapter.submitData(lifecycle,it)
+        }
+    }
+
+    private fun setRecyclerView() {
+        binding.movieRecycler.apply{
+            adapter = movieAdapter
+            layoutManager = GridLayoutManager(requireContext(),2)
+        }
     }
 }
 
