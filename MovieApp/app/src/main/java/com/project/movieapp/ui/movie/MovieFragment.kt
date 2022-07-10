cc4d75ef056303d273da22f659a18eec758501f3
wsb7788 <wsb0514@naver.com>
2022-07-10 20:28:23 +0900

    glide, recycler view holder xml 생성

--- MovieApp/app/src/main/java/com/project/movieapp/ui/movie/MovieFragment.kt
+++ MovieApp/app/src/main/java/com/project/movieapp/ui/movie/MovieFragment.kt
@@ -6,11 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.project.movieapp.R
+import com.project.movieapp.databinding.FragmentMovieBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MovieFragment : Fragment() {
 
+
+    lateinit var binding : FragmentMovieBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -21,7 +24,13 @@ class MovieFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
+        binding=  FragmentMovieBinding.inflate(inflater, container, false)
+
         return inflater.inflate(R.layout.fragment_movie, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 }
 
---
+++ MovieApp/app/src/main/java/com/project/movieapp/utils/BindingAdapters.kt
@@ -0,0 +1,11 @@
+package com.project.movieapp.utils
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+@BindingAdapter("load")
+fun loadImage(view: ImageView, url: String){
+
+    Glide.with(view).load(url).into(view)
+}
