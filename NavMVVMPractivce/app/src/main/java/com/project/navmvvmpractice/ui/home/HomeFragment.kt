42dd5436900ba9327ead2377b3df7987845b0537
wsb7788 <wsb0514@naver.com>
2022-07-25 20:52:21 +0900

    toolbar icon + navigation

--- NavMVVMPractivce/app/src/main/java/com/project/navmvvmpractice/ui/home/HomeFragment.kt
+++ NavMVVMPractivce/app/src/main/java/com/project/navmvvmpractice/ui/home/HomeFragment.kt
@@ -1,23 +1,18 @@
 package com.project.navmvvmpractice.ui.home
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.NavController
-import androidx.navigation.Navigation.findNavController
-import androidx.navigation.findNavController
+import android.view.*
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
 import com.project.navmvvmpractice.BaseFragment
 import com.project.navmvvmpractice.R
 import com.project.navmvvmpractice.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
 
 @AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
@@ -25,10 +20,31 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
 
     override fun initView() {
+        initNav()
+        initToolbar()
+    }
+
+    private fun initNav() {
         val asd = childFragmentManager.findFragmentById(R.id.fcv_home) as NavHostFragment
         NavigationUI.setupWithNavController(binding.navHome,asd.navController)
 
     }
+
+    private fun initToolbar() {
+        binding.tbHome.setNavigationIcon(R.drawable.ic_menu_icon)
+        binding.tbHome.setNavigationOnClickListener{
+
+        }
+
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
 
 
 }
